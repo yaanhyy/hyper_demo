0a3beaaea6956aa156d0b9a3ee65db8b51e61984
yaanhyy <yaanhyy@gmail.com>
2020-05-28 00:22:54 +0800

    new tcp

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use futures_util::TryStreamExt;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
-
+mod async_tcp;
 /// This is our service handler. It receives a Request, routes on its
 /// path, and returns a Future of a Response.
 async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
@@ -73,7 +73,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let server = Server::bind(&addr).serve(service);
 
     println!("Listening on http://{}", addr);
-    let i = 0;
+
     server.await?;
 
     Ok(())
